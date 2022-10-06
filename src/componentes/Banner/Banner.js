a8998797840e2c9697e6df0460f712faf3dd0ee6
Lucas <lucasmeinen@yahoo.com.br>
2022-10-06 00:59:35 -0300

    Iniciando projeto organo

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    // JSX
+    return(
+        <header className='banner'>
+            <img src="/images/banner.png"  alt="O banner principal da página do Organo"/>
+        </header>
+    )
+}
+
+export default Banner
