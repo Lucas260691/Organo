cd99e7cf2bdfd27491216c173b4af9d6c1e41b7e
Lucas <lucasmeinen@yahoo.com.br>
2022-10-06 15:21:34 -0300

    :sparkles: feat: Component ListaSuspensa

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,8 +1,20 @@
 import InputText from '../InputText'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 
 const Formulario = () => {
+    
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+    
     return (
         <section className='formulario'>
             <form>
@@ -10,6 +22,7 @@ const Formulario = () => {
                 <InputText label="Nome" placeholder="Digite Seu nome" />
                 <InputText label="Cargo" placeholder="Digite seu cargo" />
                 <InputText label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa itens={times} />
             </form>
         </section>
     )
