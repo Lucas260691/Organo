77b0749641e74859d968a24ff8e398c88645413e
Lucas <lucasmeinen@yahoo.com.br>
2022-10-09 18:31:39 -0300

    :recycle: refactor: Refatorando função time para add keys no componente Time

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -9,7 +9,8 @@ const Time = (props) => {
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
         <div className='colaboradores'>
             {props.colaboradores.map( colaborador => 
-            <Colaborador 
+            <Colaborador
+                key={colaborador.nome}
                 nome={colaborador.nome} 
                 cargo={colaborador.cargo}
                 imagem={colaborador.imagem}
