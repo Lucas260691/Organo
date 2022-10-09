adb18c36a70e9e2abd2ad758e26dae4b85a4ff69
Lucas <lucasmeinen@yahoo.com.br>
2022-10-09 18:48:30 -0300

    :lipstick: feat: Estilizaçao CSS do Componente Colaborador

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -14,6 +14,7 @@ const Time = (props) => {
                 nome={colaborador.nome} 
                 cargo={colaborador.cargo}
                 imagem={colaborador.imagem}
+                corDeFundo={props.corPrimaria}
             />)}
         </div>   
         </section>
