ecd8654a527abc9b175523bfa05d8ec8e451cbdd
Lucas <lucasmeinen@yahoo.com.br>
2022-10-09 17:24:37 -0300

    :recycle: refactor: Passando um ternário de condição para o colaborador

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -5,7 +5,7 @@ const Time = (props) => {
     const css = {backgroundColor: props.corSecundaria}
     
     return (
-        <section className='time' style={css}>
+        (props.colaboradores.length > 0) ? <section className='time' style={css}>
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
         <div className='colaboradores'>
             {props.colaboradores.map( colaborador => 
@@ -16,6 +16,7 @@ const Time = (props) => {
             />)}
         </div>   
         </section>
+        : ''
     )
 }
 
