7156341453ea754b55d2e54b8e9d415eb857ff57
Lucas <lucasmeinen@yahoo.com.br>
2022-10-09 00:26:49 -0300

    :sparkles: feat: Criando componente Colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = (props) => {
+    return (
+        <div>
+            <div>
+                <img src='https://github.com/Lucas260691.png' alt='Lucas Sodré'/>
+            </div>
+            <div>
+                <h4>Lucas Sodré</h4>
+                <h5>Desenvolvedor de Software</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
