011026fe37e329dfb7b53e61000229903d7a61a7
Lucas <lucasmeinen@yahoo.com.br>
2022-10-09 00:43:28 -0300

    :lipstick: feat: Estilizaçao CSS do Componente Colaborador

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -2,11 +2,11 @@ import './Colaborador.css'
 
 const Colaborador = (props) => {
     return (
-        <div>
-            <div>
+        <div className='colaborador'>
+            <div className='cabecalho'>
                 <img src='https://github.com/Lucas260691.png' alt='Lucas Sodré'/>
             </div>
-            <div>
+            <div className='rodape'>
                 <h4>Lucas Sodré</h4>
                 <h5>Desenvolvedor de Software</h5>
             </div>
